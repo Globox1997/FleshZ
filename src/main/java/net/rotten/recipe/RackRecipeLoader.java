6df1ad826635d735ea30aa4e239c39dfec038611
Globox1997 <mitschke11@web.de>
2022-06-10 15:07:35 +0200

    two ways to load

--- src/main/java/net/rotten/recipe/RackRecipeLoader.java
+++ src/main/java/net/rotten/recipe/RackRecipeLoader.java
@@ -2,6 +2,7 @@ package net.rotten.recipe;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,6 +12,7 @@ import com.google.gson.JsonParser;
 
 import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.minecraft.item.Item;
+import net.minecraft.resource.Resource;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -25,9 +27,11 @@ public class RackRecipeLoader implements SimpleSynchronousResourceReloadListener
 
     @Override
     public void reload(ResourceManager manager) {
-        for (Identifier id : manager.findResources("rack_items", path -> path.endsWith(".json"))) {
+
+        for (Map.Entry<Identifier, Resource> entry : manager.findResources("rack_items", id -> id.getPath().endsWith(".json")).entrySet()) {
+            Identifier id = entry.getKey();
             try {
-                InputStream stream = manager.getResource(id).getInputStream();
+                InputStream stream = entry.getValue().getInputStream();
                 JsonObject data = JsonParser.parseReader(new InputStreamReader(stream)).getAsJsonObject();
 
                 if (Registry.ITEM.get(new Identifier(data.get("item").getAsString())).toString().equals("air")) {
@@ -51,6 +55,33 @@ public class RackRecipeLoader implements SimpleSynchronousResourceReloadListener
                 LOGGER.error("Error occurred while loading resource {}. {}", id.toString(), e.toString());
             }
         }
+
+        manager.findResources("rack_items", id -> id.getPath().endsWith(".json")).forEach((id, resourceRef) -> {
+            try {
+                InputStream stream = resourceRef.getInputStream();
+                JsonObject data = JsonParser.parseReader(new InputStreamReader(stream)).getAsJsonObject();
+
+                if (Registry.ITEM.get(new Identifier(data.get("item").getAsString())).toString().equals("air")) {
+                    LOGGER.info("{} is not a valid item identifier at resouce {}", data.get("item").getAsString(), id.toString());
+                    return;
+                }
+                if (Registry.ITEM.get(new Identifier(data.get("result").getAsString())).toString().equals("air")) {
+                    LOGGER.info("{} is not a valid item identifier at resouce {}", data.get("result").getAsString(), id.toString());
+                    return;
+                }
+                if (data.get("time").getAsInt() < 0) {
+                    LOGGER.info("{} is not a valid time at resouce {}", data.get("time").getAsInt(), id.toString());
+                    return;
+                }
+
+                RecipeInit.RACK_ITEM_LIST.add((Item) Registry.ITEM.get(new Identifier(data.get("item").getAsString())));
+                RecipeInit.RACK_RESULT_ITEM_LIST.add((Item) Registry.ITEM.get(new Identifier(data.get("result").getAsString())));
+                RecipeInit.RACK_RESULT_TIME_LIST.add(data.get("time").getAsInt());
+
+            } catch (Exception e) {
+                LOGGER.error("Error occurred while loading resource {}. {}", id.toString(), e.toString());
+            }
+        });
     }
 
 }
